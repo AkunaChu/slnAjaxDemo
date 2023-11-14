d9fbce20d2ea3d13110f6407a99b05504241aec5
老朱 <vincent41103@gmail.com>
2023-11-14 18:15:36 +0800

    加入專案檔案。

---
+++ prjAjaxDemo/Controllers/ApiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjAjaxDemo.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index(string? name,int age = 20)
+        {
+            if (string.IsNullOrEmpty(name))
+                name = "guest";
+            return Content($"Hellow! {name}, age = {age}");
+
+        }
+
+        public IActionResult HW1()
+        {
+
+            return View();
+        }
+    }
+}
